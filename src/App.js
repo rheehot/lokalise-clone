3f224929acaca2500291466188997c08938ef71f
jinlee <vito9270@naver.com>
2022-05-31 16:07:25 +0900

    두개 인자 받아 사칙연산

--- src/App.js
+++ src/App.js
@@ -1,25 +1,97 @@
-import logo from './logo.svg';
-import './App.css';
+import React,  { useState, useEffect, useCallback, useMemo }  from 'react'
+import './App.css'
+import { StyledButton, StyledInput, StyledResult } from './AppStyled'
 
 function App() {
+  const [result, setResult] = useState(0)
+  const [firstElement, setFirstElement] = useState('')
+  const [secondElement, setSecondElement] = useState('')
+   
+  const onFirstChange = useCallback ( e => {
+    setFirstElement(e.target.value)
+    // console.log("first",e.target.value)
+    // console.log(firstElement)
+  },[setFirstElement]);
+  
+  const onSecondChange = useCallback ( e => {
+    setSecondElement(e.target.value)
+    // console.log("second",e.target.value)
+    // console.log(secondElement)
+  },[setSecondElement]);
+
+  const plus = useCallback (() => {
+    
+    if(firstElement === '' || isNaN(firstElement)){
+      alert("첫번째 인자를 확인하세요.")
+    }
+    else if(secondElement === '' || isNaN(secondElement)){
+      alert("두번째 인자를 확인하세요.")
+    }
+    else {
+
+      setResult(Number(firstElement) + Number(secondElement))
+    }
+  },[firstElement, secondElement, setResult])
+
+  const minus = useCallback (() => {
+    
+    if(firstElement === '' || isNaN(firstElement)){
+      alert("첫번째 인자를 확인하세요.")
+    }
+    else if(secondElement === '' || isNaN(secondElement)){
+      alert("두번째 인자를 확인하세요.")
+    }
+    else {
+
+      setResult(Number(firstElement) - Number(secondElement))
+    }
+  },[firstElement, secondElement, setResult])
+
+  const multiple = useCallback (() => {
+    
+    if(firstElement === '' || isNaN(firstElement)){
+      alert("첫번째 인자를 확인하세요.")
+    }
+    else if(secondElement === '' || isNaN(secondElement)){
+      alert("두번째 인자를 확인하세요.")
+    }
+    else {
+
+      setResult(Number(firstElement) * Number(secondElement))
+    }
+  },[firstElement, secondElement, setResult])
+
+
+  const divide = useCallback (() => {
+    
+    if(firstElement === '' || isNaN(firstElement)){
+      alert("첫번째 인자를 확인하세요.")
+    }
+    else if(secondElement === '' || isNaN(secondElement)){
+      alert("두번째 인자를 확인하세요.")
+    }
+    else if(secondElement === '0') {
+      alert("0으로 나눌 수 없습니다.")
+    }
+    else {
+
+      setResult(Number(firstElement) / Number(secondElement))
+    }
+  },[firstElement, secondElement, setResult])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <StyledInput onChange={onFirstChange}/>
+    <div>
+      <StyledButton onClick={plus}>+</StyledButton>
+      <StyledButton onClick={minus}>-</StyledButton>
+      <StyledButton onClick={multiple}>*</StyledButton>
+      <StyledButton onClick={divide}>/</StyledButton>
     </div>
+    <StyledInput onChange={onSecondChange}/>
+
+    <StyledResult>result = {result}</StyledResult>
+    </>
   );
 }
-
 export default App;
---
+++ src/AppStyled.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+
+export const StyledButton = styled.button`
+    border-radius: 8px;
+    padding: 5px 15px;
+    margin: 20px;
+    font-size: 2rem;
+`
+
+export const StyledInput = styled.input`
+    font-size: 2rem;
+`
+
+export const StyledResult = styled.div`
+    margin: 10px;
+    font-size: 4rem;
+`
